8fb82efd84a26c5b236a98c66140a173fcbae7bc
contour66 <zamir.thind@gmail.com>
2017-02-15 18:08:34 -0800

    Updated timer and first question. Adjusted some styling with fonts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -105,45 +105,66 @@
       correct: 0,    // Stores number of wins
       incorrect: 0,  // Stores number of losses
       answer: null, 
+      count: 10,
        //  userNumber: null, //Stores user number as it progresses.
   
-      gameCorrect: function (){
+      correct: function (){
         this.correct += 1;
+        clearInterval(this.counter);
+        this.count = null;
+
+         $("#timer").text(0);
+         $(".answers").remove();
+        $("#question").text("Correct!");
+
       },
 
-      gameIncorrect: function() {
+      incorrect: function() {
         this.incorrect += 1;
+         clearInterval(this.counter);
+        this.count = null;
+        $(".answers").remove();
+        $("#question").text("Inorrect!");
+        $("#timer").text(0);
+
       },
+
+  
+      timer: function () {
+        if(this.count > 0) {
+          console.log(game.count);
+          $("#timer").text( this.count);
+          this.count--;
+        } 
+        else if (this.count === 0){
+           console.log(game.count);
+          $("#timer").text( this.count);
+          clearInterval(this.counter);
+          $(".answers").remove();
+          $("#question").text("Times Up!");
+   
+         }
+      },
+  
+      counter: function() {
+        //we return the setInterval object to be found to this.counter
+        return setInterval(function() {
+      
+        //since "this" is pointing to window, we have to pass the object's name
+        game.timer();
+      
+      }, 1000);
+    }
+  };
+
+ 
 
       // displayScore: function() {
 
       // }
-    };
+    // };
 
-//     var timer = {
 
-//       count: 10,
-
-//       counter: setInterval(timer, 1000),
-
-//         runTimer: function(){
-          
-//          this.count=this.count-1;
-//           console.log(this.count);
-//           if (this.count <= 0)
-//           {
-//           clearInterval(counter);
-//           alert("times up")
-//           }
-
-//           $("#timer").text("Time Remaining " + this.count);
-
-//         }
-
-//      };
-//       // }
-  
-// timer.runTimer();
  
 
 
@@ -156,84 +177,52 @@
 
   quest1.askQuestion();
 
-  var count=5;
+  game.timer();
+  game.counter();
 
-  var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
-
-  function timer()
-  {
-    count=count-1;
-    if (count <= 0)
-    {
-     clearInterval(counter);
-     alert("times up")
-    }
-
-    $("#timer").text( count);
-
-  }
-
-//check for on answrs
+  
 
   $(".answers button").on("click", function(){
-  var answerClick= $(this).attr("id");
-  console.log("something " + answerClick);
-  });
+    
+    var answerClick= $(this).attr("id");
+    
+    console.log("something " + answerClick);
   
+    /*If the button clicked has the id of numberThree(correct answer for this question)
+    then remove the answers and show correct as a message! Stop the counter and make it zero*/
+    if (answerClick === "numberThree"){  
+      game.correct();
+      // $(".answers").remove();
+      // $("#question").text("Correct!");
+      // clearInterval(counter);
+      $("#timer").text(0);
+      
+    }
+
+    else if (answerClick !== "numberThree"){   
+      
+      game.incorrect();
+
+      // clearInterval(counter);
+      setTimeout(cambodia, 2000);
+      function cambodia(){
+        $("#question").text("Cambodia");
+      }; 
+
+    }
+  });
+    
+
+     
+
+  
+
+
+
+
+
 
 
 });
 
 
-//   //Runs start game method and clears wins losses.
-//   game.startGame();
-  
-//   //Runs method to generate random values for jewels.
-//   randomJewel.initJewels();
-
-//   // This function looks for a click among the jewels buttons.
-//   $("#jewels button").on("click", function() {
-
-//     //This variable selects the specific button pressed (this) and gets the value 
-//     //through the id (attr)
-//     var gem = $(this).attr("id");
-    
-//     //Logs the color of the jewel that is clicked
-//     console.log("Color: " + gem);
-
-//     //Logs the value of the jewel that is clicked
-//     console.log("Color value: " + randomJewel[gem]);
-
-//     //This takes the current userNumber and adds it to the old 
-//     //userNumber to create the new userNumber 
-//     game.userNumber += randomJewel[gem];
-
-//     console.log("New user number: " + game.userNumber);
-
-//     //Checks if the current user number matches the jewel number
-//     if (game.userNumber === randomJewel.magicNumber){
-//       $("#userNumber").text(game.userNumber);  //Appends the user number.
-//       game.winGame();                          //Runs method to add 1 win.
-//       console.log("Game won");
-//       game.reset();                            //Runs method to reset game.
-//       randomJewel.initJewels();                //Runs method to randomize jewels.
-//     }
-    
-//     else if (game.userNumber > randomJewel.magicNumber) {
-//       $("#userNumber").text(game.userNumber);  //Appends the user number.
-//       game.loseGame();                         //Runs method to add 1 loss.
-//       console.log("Game lost");               
-//       game.reset();                            //Runs method to reset game.
-//       randomJewel.initJewels();                //Runs method to randomize jewels.
-//     }
-    
-//     else {
-//        $("#userNumber").text(game.userNumber); //Appends the user number.
-//     }
-
-
-
-
-// });
-
-	
